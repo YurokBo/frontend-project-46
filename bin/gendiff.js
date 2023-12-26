df31beffb46afd6f0d7af628a5dccbe113fe6acd
Yuriy Bondaryuk <36164609+YurokBo@users.noreply.github.com>
2023-12-26 22:00:03 +0300

    main step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,4 @@
 #!/usr/bin/env node
-// import { program } from "commander";
-//
-// program
-//   .name('gendiff')
-//   .description('Compares two configuration files and shows a difference.')
-//   .version('1.0.0');
-//
-// program
-//   .option('-V, --version', 'output the version number')
-//   .option('-h, --help', 'output usage information')
-//   .parse();
-
 import gendiff from "../gendiff.js";
 
 gendiff();
--- gendiff.js
+++ gendiff.js
@@ -1,16 +1,21 @@
-import { program } from "commander";
+import { Command } from "commander";
+
+const program = new Command();
 
 const gendiff = () => {
   program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
-
-  program.command('gendiff')
-    .option('-V, --version', 'output the version number')
+    .version('1.0.0')
+    .argument('filepath1')
+    .argument('filepath2')
     .option('-h, --help', 'output usage information')
+    .option('-f, --format', 'output format')
+    .outputHelp()
 
-  program.parse();
+  // if (program.help) {
+  //   program.outputHelp();
+  // }
 }
 
 export default gendiff;
