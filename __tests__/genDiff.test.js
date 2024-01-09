47258d1bca090a2cee4f81bf497df7450f6c7d49
Yuriy Bondaryuk <36164609+YurokBo@users.noreply.github.com>
2024-01-09 11:02:02 +0300

    main step 5

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -6,8 +6,10 @@ import { expectedDiff } from '../__fixtures__/expected.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const file1 = path.join(__dirname, '..', '__fixtures__', 'file1.json'); // Укажите путь к вашему файлу file1.json
-const file2 = path.join(__dirname, '..', '__fixtures__', 'file2.json'); // Укажите путь к вашему файлу file2.json
+const getPath = (file) => path.join(__dirname, '..', '__fixtures__', file);
+
+const file1 = getPath('file1.json');
+const file2 = getPath('file2.json');
 
 test('compare two JSON files and generate diff', () => {
   expect(genDiff(file1, file2)).toEqual(expectedDiff);
