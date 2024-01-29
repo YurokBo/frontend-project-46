704a1b10e13fe540b4db07060bec0111ebe6ed5e
Yuriy Bondaryuk <36164609+YurokBo@users.noreply.github.com>
2024-01-29 23:36:54 +0300

    main refactoring

--- src/parsers.js
+++ src/parsers.js
@@ -3,15 +3,11 @@ import yaml from 'js-yaml';
 const getFileParser = (ext) => {
   const ymlFormatExtensions = ['.yaml', '.yml'];
 
-  if (ext === '.json') {
-    return JSON.parse;
-  }
-
   if (ymlFormatExtensions.includes(ext)) {
     return yaml.load;
   }
 
-  throw new Error('Unknown extension');
+  return JSON.parse;
 };
 
 export default getFileParser;
