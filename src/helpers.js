8520881e3ef49fdc12669fc619ef9b87bcf6b124
Yuriy Bondaryuk <36164609+YurokBo@users.noreply.github.com>
2024-01-30 10:16:53 +0300

    main refactoring

--- src/helpers.js
+++ src/helpers.js
@@ -14,10 +14,9 @@ export const getFileExt = (file) => path.extname(file);
 export const createDiffTree = (filepath1, filepath2) => {
   const keys1 = Object.keys(filepath1);
   const keys2 = Object.keys(filepath2);
-  const keys = _.union(keys1, keys2);
-  const sortedKeys = [...keys].sort();
+  const keys = _.sortBy(_.union(keys1, keys2));
 
-  return sortedKeys.map((key) => {
+  return keys.map((key) => {
     if (_.isObject(filepath1[key]) && _.isObject(filepath2[key])) {
       return {
         key, type: 'nested', children: createDiffTree(filepath1[key], filepath2[key]),
