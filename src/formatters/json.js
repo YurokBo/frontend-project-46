de8c2cd2b8a6c828b51209134b5349b61e645988
Yuriy Bondaryuk <36164609+YurokBo@users.noreply.github.com>
2024-01-30 09:53:32 +0300

    main refactoring

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -14,24 +14,25 @@ const formatValue = (value, depth = 0, replacer = ' ') => {
 };
 
 const json = (value, replacer = ' ') => {
-  const iter = (currentValue, depth = 0) => currentValue.map((item) => {
-    const entries = Object.entries(item);
-    const bracketIndent = replacer.repeat((depth * 2));
-
-    const lines = entries.map(([key, val]) => {
-      const currentIndent = replacer.repeat((depth * 2) + 2);
-
-      if (key === 'children') {
-        return `${currentIndent}"${key}": [\n${iter(val, depth + 2).join('\n')}\n${currentIndent}],`;
-      }
-
-      return `${currentIndent}"${key}": ${formatValue(val, depth + 1)}`;
-    });
-
-    return `${bracketIndent}{\n${lines.join('\n')}\n${bracketIndent}},`;
-  });
-
-  return `[\n${iter(value, 1).join('\n')}\n]`;
+  return JSON.stringify(value);
+  // const iter = (currentValue, depth = 0) => currentValue.map((item) => {
+  //   const entries = Object.entries(item);
+  //   const bracketIndent = replacer.repeat((depth * 2));
+  //
+  //   const lines = entries.map(([key, val]) => {
+  //     const currentIndent = replacer.repeat((depth * 2) + 2);
+  //
+  //     if (key === 'children') {
+  //       return `${currentIndent}"${key}": [\n${iter(val, depth + 2).join('\n')}\n${currentIndent}],`;
+  //     }
+  //
+  //     return `${currentIndent}"${key}": ${formatValue(val, depth + 1)}`;
+  //   });
+  //
+  //   return `${bracketIndent}{\n${lines.join('\n')}\n${bracketIndent}},`;
+  // });
+  //
+  // return `[\n${iter(value, 1).join('\n')}\n]`;
 };
 
 export default json;
