fda0161fad0138d05f72eefeffdbbd6c16ff295a
Yuriy Bondaryuk <36164609+YurokBo@users.noreply.github.com>
2024-02-18 18:14:48 +0300

    main fix stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,58 +1,47 @@
 import _ from 'lodash';
 
-const formatValue = (value, depth = 0, replacer = ' ') => {
-  if (_.isObject(value)) {
-    const currentIndent = replacer.repeat(depth * 4);
-    const bracketIndent = replacer.repeat((depth - 1) * 4);
-    const keys = Object.keys(value);
-    const formattedValue = keys.map((key) => `${currentIndent}${key}: ${formatValue(value[key], depth + 1)}`);
+const makeIndent = (count) => `${'  '.repeat(count * 2)}`;
 
-    return `{\n${formattedValue.join('\n')}\n${bracketIndent}}`;
+const formatValue = (value, depth = 0) => {
+  if (_.isObject(value)) {
+    const keys = Object.keys(value);
+    const formattedValue = keys.map((key) => `${makeIndent(depth + 1)}${key}: ${formatValue(value[key], depth + 1)}`);
+
+    return `{\n${formattedValue.join('\n')}\n${makeIndent(depth)}}`;
   }
 
   return value;
 };
 
-const getStylishDiff = (value, replacer = ' ') => {
-  const leftShiftTypes = ['added', 'removed', 'updated'];
+const getStylishDiff = (value, depth = 0) => {
+  const lines = value.map((item) => {
+    if (item.type === 'nested') {
+      return `${makeIndent(depth + 1)}${item.key}: ${getStylishDiff(item.children, depth + 1)}`;
+    }
 
-  const iter = (currentValue, depth = 0) => {
-    const bracketIndent = replacer.repeat((depth - 1) * 4);
+    if (item.type === 'added') {
+      return `  ${makeIndent(depth)}+ ${item.key}: ${formatValue(item.value, depth + 1)}`;
+    }
 
-    const lines = currentValue.map((item) => {
-      const leftShift = leftShiftTypes.includes(item.type) ? 2 : 0;
-      const currentIndent = replacer.repeat(depth * 4 - leftShift);
+    if (item.type === 'removed') {
+      return `  ${makeIndent(depth)}- ${item.key}: ${formatValue(item.value, depth + 1)}`;
+    }
 
-      if (item.type === 'nested') {
-        return `${currentIndent}${item.key}: ${iter(item.children, depth + 1)}`;
-      }
+    if (item.type === 'updated') {
+      return [
+        `  ${makeIndent(depth)}- ${item.key}: ${formatValue(item.value1, depth + 1)}`,
+        `  ${makeIndent(depth)}+ ${item.key}: ${formatValue(item.value2, depth + 1)}`,
+      ].join('\n');
+    }
 
-      if (item.type === 'added') {
-        return `${currentIndent}+ ${item.key}: ${formatValue(item.value, depth + 1)}`;
-      }
+    if (item.type === 'unchanged') {
+      return `${makeIndent(depth + 1)}${item.key}: ${formatValue(item.value, depth + 1)}`;
+    }
 
-      if (item.type === 'removed') {
-        return `${currentIndent}- ${item.key}: ${formatValue(item.value, depth + 1)}`;
-      }
+    return null;
+  });
 
-      if (item.type === 'updated') {
-        return [
-          `${currentIndent}- ${item.key}: ${formatValue(item.value1, depth + 1)}`,
-          `${currentIndent}+ ${item.key}: ${formatValue(item.value2, depth + 1)}`,
-        ].join('\n');
-      }
-
-      if (item.type === 'unchanged') {
-        return `${currentIndent}${item.key}: ${formatValue(item.value, depth + 1)}`;
-      }
-
-      return null;
-    });
-
-    return `{\n${lines.join('\n')}\n${bracketIndent}}`;
-  };
-
-  return iter(value, 1);
+  return `{\n${lines.join('\n')}\n${makeIndent(depth)}}`;
 };
 
 export default getStylishDiff;
